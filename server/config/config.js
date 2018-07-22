4b73a71d5e0dd55514d105da3d7cbbce640814b0
Programito <Programito@gmail.com>
2018-07-22 17:40:36 +0200

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,25 @@
 //Puerto
 //==================================
 process.env.PORT = process.env.PORT || 3000;
+
+//==================================
+// Entorno
+//==================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==================================
+// Base de datos
+//==================================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:vetetuasaber2@ds247191.mlab.com:47191/kafe';
+}
+
+
+
+process.env.URLDB = urlDB;
